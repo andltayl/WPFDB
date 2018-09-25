802e98e6e2a526ec8800444ad7a116d019154418
Anderson Taylor <andltayl@ut.utm.edu>
2018-09-25 11:23:33 -0500

    Changed mainwindow title and added comments/README.

--- WPFDB/MainWindow.xaml.cs
+++ WPFDB/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Author: Anderson Taylor
+ * Date: 9/25/2018
+ * File: MainWindow.xaml.cs
+ * Description: Logic for the database reader window.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +31,7 @@ namespace WPFDB
         public MainWindow()
         {
             InitializeComponent();
-            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\atayl\\source\\repos\\WPFDB\\WPFDB\\CW5.accdb");
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\atayl\\source\\repos\\WPFDB\\WPFDB\\CW5.accdb"); //Set up connection to database
         }
 
         private void See_Assets_Button_Click(object sender, RoutedEventArgs e)
@@ -38,7 +43,7 @@ namespace WPFDB
             string data = "";
             while (read.Read())
             {
-                for(int i = 0; i < 3; i++)
+                for(int i = 0; i < 3; i++)      //Read out 3 columns in each row.
                 {
                     data += read[i].ToString() + "  ";
                 }
@@ -46,13 +51,13 @@ namespace WPFDB
                 data += "\n";
 
             }
-            cn.Close();
+            cn.Close();     //Added this so that buttons could be pressed multiple times without a crash.
             DB_Display_Box.Text = data;
         }
 
         private void See_Employees_Button_Click(object sender, RoutedEventArgs e)
         {
-            string query = "select * from Employees";
+            string query = "select * from Employees";       //This is the only difference between the above button and this one.
             OleDbCommand cmd = new OleDbCommand(query, cn);
             cn.Open();
             OleDbDataReader read = cmd.ExecuteReader();
---
+++ WPFDB/README.txt
@@ -0,0 +1,9 @@
+﻿Author: Anderson Taylor
+Date: 9/24/2018
+Classwork 5	CSCI352
+Description: This program reads and displays data from a database file.
+
+Following the instructions from the tutorial I created the interface and button logic. Then I changed the colors on the backgrounds 
+to make them a little more eye friendly.
+
+To run program simply open is VS and run.
